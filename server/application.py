2fd6be7a13c0e939655c3923416a7583417f991c
WoothAmwar <anwar09102005@gmail.com>
2024-12-18 21:26:18 -0500

    AWS Down? Commented out cron jobs

--- server/application.py
+++ server/application.py
@@ -80,24 +80,24 @@ def isUpdateTime(upd_hour, upd_min, upd_sec, useModulus=False, isDevelopment=Fal
 
 # ----------
 
-@scheduler.task('cron', id='youtube_job', hour=15, minute=30, second=0)
-def youtube_job():
-    """
-    Calls the complete_reload function at a specific time every day
-    :return: None
-    """
-    # print("Doing the thing")
-    mongo_insert_test(calledAsIntended=False)
-    all_user_google_ids = get_all_user_google()
-    for googleID in all_user_google_ids:
-        complete_reload(googleID, doReturn=False)
-    # with scheduler.app.app_context():
-    # complete_reload(doReturn=False)
-
-@scheduler.task('cron', id='malw_put_job', hour=15, minute=45, second=0)
-def malw_job():
-    r = requests.post("https://malw-api.onrender.com/api", timeout=300)
-    print(r.json())
+# @scheduler.task('cron', id='youtube_job', hour=15, minute=30, second=0)
+# def youtube_job():
+#     """
+#     Calls the complete_reload function at a specific time every day
+#     :return: None
+#     """
+#     # print("Doing the thing")
+#     mongo_insert_test(calledAsIntended=False)
+#     all_user_google_ids = get_all_user_google()
+#     for googleID in all_user_google_ids:
+#         complete_reload(googleID, doReturn=False)
+#     # with scheduler.app.app_context():
+#     # complete_reload(doReturn=False)
+#
+# @scheduler.task('cron', id='malw_put_job', hour=15, minute=45, second=0)
+# def malw_job():
+#     r = requests.post("https://malw-api.onrender.com/api", timeout=300)
+#     print(r.json())
 
 t = ""
 info = ""
